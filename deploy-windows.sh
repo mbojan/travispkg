1f3c261fbeb0c845f7670fffa6691f00ff638e97
Michal Bojanowski <michal2992@gmail.com>
2020-02-14 03:59:08 +0100

    Deploy on Windows

---
+++ deploy-windows.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+PKG_REPO=$PWD
+
+cd ..; mkdir drat; cd drat
+
+## Set up Repo parameters
+git init
+git config user.name "Build Pusher"
+git config user.email "michal2992@example.com"
+git config --global push.default simple
+
+## Get drat repo
+git remote add upstream "https://$GH_TOKEN@github.com/mbojan/drat.git"
+git fetch upstream 2>err.txt
+git checkout gh-pages
+
+Rscript -e "drat::insertPackage('$PKG_REPO/$PKG_ZIP', \
+repodir = '.', \
+commit='Travis update: build $TRAVIS_BUILD_NUMBER on Windows')"
+git push 2> /tmp/err.txt
+
