c35aec783e62d807312356945b9ebba6516bd00a
Michal Bojanowski <michal2992@gmail.com>
2020-02-19 22:57:35 +0100

    Add testthat test

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(travispkg)
+
+test_check("travispkg")
---
+++ tests/testthat/test-fun.R
@@ -0,0 +1,6 @@
+context("Testing fun()")
+
+
+test_that("fun () works", {
+            expect_identical(1, fun(1))
+})
